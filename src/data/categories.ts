7423dd3a05c9b48f4df935bea33af141fc3144ae
greeneley <anhchangtoanhoc97@gmail.com>
2024-07-20 16:35:13 +0700

    blog: fix bug

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,2 +1,2 @@
 // List of categories for blog posts
-export const CATEGORIES = ['React', 'Git', 'Algorithm', 'HTML/CSS', 'Other'] as const
+export const CATEGORIES = ['React', 'Git', 'Algorithm', 'HTML CSS', 'Other'] as const
